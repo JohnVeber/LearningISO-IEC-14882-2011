fdc7cb23e57021e82b6611246bcb7f267677eb51
JohnVeber <evgeniy.weber@gmail.com>
2026-03-16 22:09:08 +0300

    Ex.1.22 from Chapter1 fixed (wrong solution were provided; Ex.2.4 added.

--- CPP_PRIMER/Chapter1/Ex.1.22.cpp
+++ CPP_PRIMER/Chapter1/Ex.1.22.cpp
@@ -2,10 +2,9 @@
 #include "Sales_item.h"
 int main(){
     Sales_item result_item, item;
-    if(std::cin >> item){
-        result_item = item; // This step was necessary for ISBN initialization, as the operator "+=" did not copy it to the "result_item" object.
+    if(std::cin >> result_item){
         while (std::cin >> item){
-        result_item += item;
+            result_item += item;
         }
         std::cout << "Sum of these transactions is:" << result_item << std::endl;
     }
