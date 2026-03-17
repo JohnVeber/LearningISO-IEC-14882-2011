9a7ca1a910fa1f27ffeeb56e088ed28668f2f6f9
JohnVeber <evgeniy.weber@gmail.com>
2026-03-17 22:26:38 +0300

    Ex.2.8 added.

---
+++ CPP_PRIMER/PART_1_BASICS/Chapter2/Ex.2.8.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(){
+    std::cout << "2M\n" << std::endl; // First part of the task
+    std::cout << "2\tM\n" << std::endl; // Second part of the task
+    return 0;
+}
