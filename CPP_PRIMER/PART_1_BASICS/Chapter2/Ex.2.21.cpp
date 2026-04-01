bacc8cf7a5f694872aa412ce5b5eeab43b2cd7b5
JohnVeber <evgeniy.weber@gmail.com>
2026-04-01 23:07:19 +0300

    Ex.2.21, 2.22 added.

---
+++ CPP_PRIMER/PART_1_BASICS/Chapter2/Ex.2.21.cpp
@@ -0,0 +1,26 @@
+/* Rows with invalid assignments are commented on. */
+#include <iostream>
+
+int main(){
+    int i = 0;
+    // Case a.
+    //double* dp = &i;
+
+    // Case b.
+    //int *ip = i;
+
+    // Case c.
+    int *p = &i;
+
+    std::cout << "Value of *p is:  " << *p << std::endl; // Expected result in this row is "Value of *p is:  "0".
+}
+
+/* **************************************************** COMMENTS: **************************************************** */
+
+/* Case a: We can’t initialize the pointer of one type by a value of the variable with another type
+ without explicit conversion (reinterpret_cast).*/
+
+/* Case b: The pointer must be initialized by an address, not a value.
+The exception is only a literal "0" (or nullptr) that can be assigned to a pointer.*/
+
+/* Case c: Correct.*/
