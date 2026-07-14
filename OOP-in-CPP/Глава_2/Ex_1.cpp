812262b4f335b72a19bb6d40af1bd9a777aabf0f
JohnVeber <evgeniy.weber@gmail.com>
2026-07-15 00:05:18 +0300

    Chapter 2 Ex.1 fixed, Ex.2,3 added.

--- OOP-in-CPP/Глава_2/Ex_1.cpp
+++ OOP-in-CPP/Глава_2/Ex_1.cpp
@@ -3,7 +3,7 @@ int main(){
     const float GALLONS_TO_CUBIC_FEET = 7.481f;
     std::cout << "Enter some number of gallons:" << std::endl;
     float gallons = 0.0f;
-    float cubicFeets 0.0f; 
+    float cubicFeets = 0.0f;
     std::cin >> gallons;
     cubicFeets = gallons * GALLONS_TO_CUBIC_FEET ;
     std::cout << "The equivalent volume in cubic feet is: " << cubicFeets << std::endl;
