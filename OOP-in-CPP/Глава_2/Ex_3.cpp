4895977a60439ea9bdb6eec5dcc144810fb5f21f
JohnVeber <evgeniy.weber@gmail.com>
2026-07-15 19:27:18 +0300

    Chapter 2 Ex.3 corrected, Ex.4 added.

--- OOP-in-CPP/Глава_2/Ex_3.cpp
+++ OOP-in-CPP/Глава_2/Ex_3.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main(){
     const int VAR = 10;
---
+++ OOP-in-CPP/Глава_2/Ex_4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    cout << "Буря мглою небо кроет," << '\n';
+    cout << "Вихри снежные крутя;" << '\n';
+    cout << "То, как зверь, она завоет," << '\n';
+    cout << "То заплачет, как дитя ..." << '\n' << endl;
+}
